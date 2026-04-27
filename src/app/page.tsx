4e21366f875aa036322063831730d1166fc27565
Luthfi08 <luthfiyahya0604@gmail.com>
2026-04-28 00:04:14 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,43 @@
+export default function learn() {
+  return (
+    <section className="h-screen bg-gray-100 flex gap-4">
+    <div className="bg-purple-300 p-8 h-64 w-72 rounded-4xl flex flex-col justify-between text-blue-950"> 
+      <div className="flex gap-4">
+        <img src="/palette.svg" alt="palette"/>
+        <p className="font-bold">Design</p>
+        </div>
+        <div>
+        <h2 className="text-4xl font-bold">Figma</h2>
+        <p className="font-bold">8 hours ago</p>
+        </div>
+        </div>
+       
+      <div className="bg-orange-300 p-8 h-64 w-72 rounded-4xl flex flex-col justify-between text-blue-950"> 
+      <div className="flex gap-4">
+        <img src="/palette.svg" alt="palette"/>
+        <p className="font-bold">Codding</p>
+        </div>
+        <div>
+        <h2 className="text-4xl font-bold">Pyhton</h2>
+        <p className="font-bold">2 days ago</p>
+        </div>
+        </div>
+       
+        <div className="bg-red-300 p-8 h-64 w-72 rounded-4xl flex flex-col justify-between text-blue-950"> 
+      <div className="flex gap-4">
+        <img src="/palette.svg" alt="palette"/>
+        <p className="font-bold">Design</p>
+        </div>
+        <div>
+        <h2 className="text-4xl font-bold">Sketch</h2>
+        <p className="font-bold">4 days ago</p>
+        </div>
+        </div>
+       
+
+        </section>
+  );
+  }
+       
+
+  
